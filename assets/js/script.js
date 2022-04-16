0a8eab846cdbcb484bab042fbf457a1badcb852a
Jennifer Schmiett <bailliejm@gmail.com>
2022-04-16 18:12:33 -0500

    Starting initial code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
-
+var dateTitle = document.querySelector("h1")
 
 var dateToday = new Date
 console.log(Date);
 
+var dateString = new Date(dateString)
+console.log(dateString)
+
 var month = dateToday.getMonth
 console.log(month);
+
+var day = dateToday.getDay
+console.log(day)
+
+var year = dateToday.getFullYear
+console.log(year)
+
+dateTitle.innerHTML(dateToday)
