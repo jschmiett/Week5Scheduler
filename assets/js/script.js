d3b24e3259ff229e858612a375de3c75b2534a48
Jennifer Schmiett <bailliejm@gmail.com>
2022-04-16 21:37:12 -0500

    Added in psuedocoding

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,14 @@ var year = dateToday.getFullYear
 console.log(year)
 
 dateTitle.innerHTML(dateToday)
+
+//Display date object as title of page
+//Loop append buttons i = 24 times
+//On click event listener for scheduling 
+//Store scheduling in memory
+//If else statement to determine button colors changing as time passes
+    //If time < time window then button is grey
+    //If time == time window then button is red
+    //Else button is green
+
+    
