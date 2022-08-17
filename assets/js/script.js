d14b987d53886840ed70d0e8fa282a6f788bcdad
Jennifer Schmiett <bailliejm@gmail.com>
2022-08-16 20:13:03 -0500

    Updated started code and included moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,12 @@
-var dateTitle = document.querySelector("h1")
 
-var dateToday = new Date
-console.log(Date);
-
-var dateString = new Date(dateString)
-console.log(dateString)
-
-var month = dateToday.getMonth
-console.log(month);
-
-var day = dateToday.getDay
-console.log(day)
-
-var year = dateToday.getFullYear
-console.log(year)
-
-dateTitle.innerHTML(dateToday)
-
+// moment().format("MMM Do YY");
 //Display date object as title of page
 //Loop append buttons i = 24 times
-//On click event listener for scheduling 
+//On click event listener for scheduling
 //Store scheduling in memory
 //If else statement to determine button colors changing as time passes
-    //If time < time window then button is grey
-    //If time == time window then button is red
-    //Else button is green
+//If time < time window then button is grey
+//If time == time window then button is red
+//Else button is green
 
-    
+$("#currentDay").text(moment().format("MMM Do YY"))
